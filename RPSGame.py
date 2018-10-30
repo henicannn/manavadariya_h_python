8713b4e24e12781e13bff27d695fbd4e5e85ffc9
henipatel8 <henipatel8@github.com>
2018-10-30 10:39:37 -0400

    RPSGame.py

--- RPSGame.py
+++ RPSGame.py
@@ -8,9 +8,6 @@ choices = ["Rock", "Paper","Scissors"]
 # make the computer choose a weapon from the choices array at random
 computer_choice = choices[randint(0,2)]
 
-c_lives = 3
-p_lives = 3
-
 # print the choice to the terminal window
 print("computer chooses: ", computer_choice)
 
@@ -24,35 +21,52 @@ while player == False:
 	player = input("Rock, Paper or Scissors?\n")
 	print(player, "\n")
 
+if player == "quit":
+	exit()
+
+	print("Player chooses:", player)
+	print("Computer chooses:", computer_choice)
+
+if player == "restart":
+	c_lives = 3
+	p_lives = 3
+	player = False
+
+	computer_choice = choices[randint(0,2)]
+
+
 if player == computer_choice: #tie
 	print("tie")
 
 elif player == "Rock":
 	if computer_choice == "Paper":
-		c_lives -=1
+		c_lives = c_lives - 1
 		print("loss")
 	else:
-		p_lives -=1
+		p_lives = p_lives - 1
 		print("win", player, "smashes", computer_choice)
 
 elif player == "Paper":
 	if computer_choice == "Scissors":
-		c_lives -=1
+		c_lives = c_lives - 1
 		print("loss")
 	else:
-		p_lives -=1
+		p_lives = p_lives - 1
 		print("win", player, "covers", computer_choice)
 
 elif player == "Scissors":
 	if computer_choice == "Rock":
-		c_lives -=1
+		c_lives = c_lives - 1
 		print("loss")
 	else:
-		p_lives -=1
+		p_lives = p_lives - 1
 		print("win", player, "cuts", computer_choice)
 
 else:
 	print("check your spelling... thats not a valid choice\n")
+
+if player == "quit":
+	print("Rock, Paper, or Scissors")
 
 # reset game loop
 	player = False
