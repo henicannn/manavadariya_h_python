095db9a1fade80c3b1a195c5b1667461f82ad54f
henipatel8 <henipatel8@github.com>
2018-10-30 08:55:34 -0400

    RPSGame.py

--- RPSGame.py
+++ RPSGame.py
@@ -8,44 +8,52 @@ choices = ["Rock", "Paper","Scissors"]
 # make the computer choose a weapon from the choices array at random
 computer_choice = choices[randint(0,2)]
 
+c_lives = 3
+p_lives = 3
+
 # print the choice to the terminal window
 print("computer chooses: ", computer_choice)
 
 #set up our loop
+player = False
 
-while player is False:
-	#set player to True by making a selection
+while player == False:
+# set player to True by making a selection
+
 	print("choose your weapon")
 	player = input("Rock, Paper or Scissors?\n")
 	print(player, "\n")
 
-	if player == computer_choice: #tie
-	    print("tie")
+if player == computer_choice: #tie
+	print("tie")
 
-	elif player == "Rock":
-		if computer_choice == "Paper":
-			print("loss")
-		else:
-			print("win", player, "smashes", computer_choice)
-
-	elif player == "Paper":
-	    if computer_choice == "Scissors":
-	    	print("loss")
-	    else:
-	    	print("win", player, "smashes", computer_choice)
-
-	elif player == "Scissors":
-	    if computer_choice == "Rock":
-	    	print("loss")
-	    else:
-	    	print("win", player, "smashes", computer_choice)
-
-	elif player == "quit":
-		exit()
+elif player == "Rock":
+	if computer_choice == "Paper":
+		c_lives -=1
+		print("loss")
 	else:
-		print("check your spelling... thats not a valid choice\n")
+		p_lives -=1
+		print("win", player, "smashes", computer_choice)
 
-	#reset game loop
-	player = Flase
+elif player == "Paper":
+	if computer_choice == "Scissors":
+		c_lives -=1
+		print("loss")
+	else:
+		p_lives -=1
+		print("win", player, "covers", computer_choice)
 
-computer_choice = choices[randint(0,2)]
+elif player == "Scissors":
+	if computer_choice == "Rock":
+		c_lives -=1
+		print("loss")
+	else:
+		p_lives -=1
+		print("win", player, "cuts", computer_choice)
+
+else:
+	print("check your spelling... thats not a valid choice\n")
+
+# reset game loop
+	player = False
+	computer_choice = choices[randint(0,2)]
