d980bb1b62732fc1cda5a062fbf3eab4c5d9a768
henipatel8 <henipatel8@github.com>
2018-10-30 23:27:26 -0400

    RPSGame.py

--- RPSGame.py
+++ RPSGame.py
@@ -3,11 +3,9 @@ from random import randint
 choices = ["Rock", "Paper", "Scissors"]
 
 computer_choice = choices[randint(0,2)]
+
 c_lives = 3
 p_lives = 3
-
-
-print("computer chooses: ", computer_choice)
 
 player = False
 
@@ -18,54 +16,52 @@ while player == False:
     print("choose your weapon")
     player = input("Rock, Paper or Scissors ?\n")
 
-if player == "quit":
-	exit()
+    if player == "quit":
+        exit()
 
-	print("Player chooses:", player)
-	print("Computer chooses:", computer_choice)
+    print("Player chooses:", player)
+    print("Computer chooses:", computer_choice)
 
-	if player == "restart":
-		c_lives = 3
-		p_lives = 3
-		
-	player = False
+    if player == "restart":
+        c_lives = 3
+        p_lives = 3
+        
+    player = False
 
-	computer_choice = choices[randint(0,2)]
+    computer_choice = choices[randint(0,2)]
 
 
-if player == computer_choice: #tie
-	print("Tie")
+    if player == computer_choice:
+        print("Tie")
 
-elif player == "Rock":
-	if computer_choice == "Paper":
-		c_lives = c_lives - 1
-		print("loss")
-	else:
-		p_lives = p_lives - 1
-		print("win", player, "smashes", computer_choice)
+    elif player == "Rock":
+        if computer_choice == "Paper":
+            c_lives = c_lives - 1
+            print("loss")
+        else:
+            p_lives = p_lives - 1
+            print("win", player, "covers", computer_choice)
 
-elif player == "Paper":
-	if computer_choice == "Scissors":
-		c_lives = c_lives - 1
-		print("loss")
-	else:
-		p_lives = p_lives - 1
-		print("win", player, "covers", computer_choice)
+    elif player == "Paper":
+        if computer_choice == "Scissors":
+            c_lives = c_lives - 1
+            print("loss")
+        else:
+            p_lives = p_lives - 1
+            print("win", player, "cuts", computer_choice)
 
-elif player == "Scissors":
-	if computer_choice == "Rock":
-		c_lives = c_lives - 1
-		print("loss")
-	else:
-		p_lives = p_lives - 1
-		print("win", player, "cuts", computer_choice)
+    elif player == "Scissors":
+        if computer_choice == "Rock":
+            c_lives = c_lives - 1
+            print("loss")
+        else:
+            p_lives = p_lives - 1
+            print("win", player, "smashes", computer_choice)
+            
 
-else:
-	print("check your spelling... thats not a valid choice\n")
+    if player == "quit":
+        exit()
 
-if player == "quit":
-	print("Rock, Paper, or Scissors")
-
-# reset game loop
-	player = False
-	computer_choice = choices[randint(0,2)]
+    # reset game loop
+        player = False
+        computer_choice = choices[randint(0,2)]
--- functions.py
+++ functions.py
@@ -16,9 +16,23 @@ greetingsWithArgs("Hey..!!!, Ssup..!!!")
 greetingsWithArgs("Something completely different.")
 greetingsWithArgs("Running it again.")
 
+# variables and scope
+myNumberVariable = 10
+
+
 # returning values from the functions (very powerful)
 def someMath(num1=2, num2=4):
+    global myNumberVariable
+
+    myNumberVariable = num1 + num2
     return num1 + num2
+
 
 sum = someMath()
 print("We are doing some math and getting", sum)
+
+sum = someMath(10, 15)
+print("another math operation with", sum, "as the result")
+
+# variables and scope
+myNumberVariable = 10
